cd1121a08e529efab0924ca3a797b2c5b0587944
kkk <857864622@qq.com>
2019-01-20 22:32:07 +0800

    'ss'

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,33 @@
+var path = require('path');
 module.exports = {
-  entry: __dirname + 'src/main.js', //入口文件
+  devtool: 'eval-source-map', //为了生成map文件
+  entry: __dirname + '/src/main.js', //入口文件，//__dirname 总是指向被执行 js 文件的绝对路径
   output: {
     path: __dirname + '/public', //打包后文件存放的地方
     filename: 'bundle.js' //打包后输出文件名
+  },
+  //开发环境服务设置
+  devServer: {
+    contentBase: "./public", //默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到“public"目录）
+    historyApiFallback: true, //在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+    inline: true, //设置为true，当源文件改变时会自动刷新页面
+    port: 8600, //设置默认监听端口，如果省略，默认为”8080“
+  },
+  module: {
+    rules: [{
+      test: /(\.jsx|\.js)$/, //用以匹配loaders所处理文件的拓展名的正则表达式（必须）
+      use: {
+        loader: "babel-loader", //loader的名称（必须）
+        options: {
+          //cacheDirectory: true, //设置是否缓存
+          //配置需要的模块。env为转换es5，react为解析react
+          presets: [
+            "env", "react"
+          ]
+        }
+      },
+      // include: /src/, //需要处理的文件(可选)
+      exclude: /node_modules/ //屏蔽不需要处理的文件(可选)
+    }]
   }
 }
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// main.js
+import React from 'react';
+import Greeter from './Greeter';
+import {
+  render
+} from 'react-dom';
+
+render( < Greeter / > , document.getElementById('root'));
